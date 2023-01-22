e013430a907af0fc6bc28b87afe261925a227f48
dksekfbs72 <dksekfbs72@naver.com>
2023-01-22 20:35:05 +0900

    [다륜] 프로젝트 배포
    배포 설정

--- leisure-api/src/main/java/com/zerobase/leisure/LeisureApplication.java
+++ leisure-api/src/main/java/com/zerobase/leisure/LeisureApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@ComponentScan(basePackages = { "com.zerobase.leisure", "com.zerobase.common" })
 public class LeisureApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LeisureApplication.class,args);
